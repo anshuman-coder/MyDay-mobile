44a002857032e72c5a8912a7125d758a0738aa96
anshuman-coder <anshurockingboy23@gmail.com>
2023-02-12 20:37:59 +0530

    small change

--- api/config/db.connection.js
+++ api/config/db.connection.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+mongoose.set('strictQuery', true);
+
 function connectionToDB(url) { 
   return new Promise((resolve, reject) => { 
     mongoose.connect(url)
